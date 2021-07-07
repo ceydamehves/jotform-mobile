1f17e0df1eaa6a58163e5d359720838603e0657f
Ceyda Keklik <cmehves.keklik@std.hku.edu.tr>
2021-07-07 11:07:15 +0300

    rule correction

--- components/__tests__/StyledText-test.js
+++ components/__tests__/StyledText-test.js
@@ -1,7 +1,11 @@
+/* eslint-disable node/no-extraneous-import */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable node/no-unsupported-features/es-syntax */
+
 import * as React from 'react';
 import renderer from 'react-test-renderer';
 
-import { MonoText } from '../StyledText';
+import { MonoText } from '../StyledText.tsx';
 
 it(`renders correctly`, () => {
   const tree = renderer.create(<MonoText>Snapshot test!</MonoText>).toJSON();
