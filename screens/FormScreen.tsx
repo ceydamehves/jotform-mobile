59a96901c8802287ad45c01cd9a4e493ce114b00
Ceyda Keklik <cmehves.keklik@std.hku.edu.tr>
2021-07-13 14:19:19 +0300

    styles added.

--- screens/FormScreen.tsx
+++ screens/FormScreen.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import { useEffect, useState } from 'react';
-import { Button, Alert } from 'react-native';
+import { StyleSheet, Alert, Dimensions, TouchableOpacity} from 'react-native';
 import axios from 'axios';
 import { Text, View } from '../components/Themed';
 
 import {apiKey} from '../secret';
 
 import { Video } from 'expo-av';
+const { width, height } = Dimensions.get("window");
 
 export default function FormScreen( ) {
 
@@ -25,27 +26,78 @@ export default function FormScreen( ) {
     <View style={{ flex: 1 }}>
       {isLoading ? <Text>Loading...</Text> : 
       (
-        <View>
-          <Text>{data['content']["1"]["text"]}</Text>
-          <Button
-              title={data['content']["1"]["options"]}
-              color="#f194ff"
-              onPress={() => Alert.alert('pressed')}
-            />
-          <Button
-              title={data['content']["1"]["options"]}
-              color="#f194ff"
-              onPress={() => Alert.alert('pressed')}
-          />
+        <View style={{ flex:1 }}>
+          
           <Video
             source={{ uri: 'https://www.w3schools.com/html/mov_bbb.mp4' }}
             shouldPlay
             isLooping
             resizeMode="cover"
-            style={{ width:300, height: 300 }}
+            style={styles.backgroundVideo}
           />
+          <Text style={styles.title}>{data['content']["1"]["text"]}</Text>
+          
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => Alert.alert('pressed')}>
+              <Text style={styles.choice}>A</Text>
+            <Text style={styles.buttonText}>{data['content']["1"]["options"]}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => Alert.alert('pressed')}>
+            <Text style={styles.choice}>B</Text>
+            <Text style={styles.buttonText}>{data['content']["1"]["options"]}</Text>
+          </TouchableOpacity>
+          
         </View>
       )} 
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  backgroundVideo: {
+    height: height,
+    position: "absolute",
+    top: 0,
+    left: 0,
+    alignItems: "stretch",
+    bottom: 0,
+    right: 0
+  },
+  title:{
+    flex:1,
+    alignSelf:'center',
+    marginTop:50,
+    color:'#fff',
+    fontSize: 28,
+    fontWeight: 'bold'
+  },
+  button:{
+    flexDirection:'row',
+    alignSelf:'center',
+    backgroundColor: 'rgba(46, 136, 91, 0.5)',
+    padding:10,
+    borderRadius:20,
+    borderColor:'rgba(46, 136, 91, 0.7)',
+    borderWidth:2,
+    marginTop:5,
+    marginBottom:5,
+    width: width*0.9
+  },
+  buttonText:{
+    color:'#fff',
+    fontSize: 16,
+  },
+  choice:{
+    textAlign:'center',
+    color:'#fff',
+    marginRight:10,
+    backgroundColor: 'rgb(46, 136, 91)',
+    borderRadius:15,
+    width:25,
+    height:25,
+    fontSize:16
+  }
+});
